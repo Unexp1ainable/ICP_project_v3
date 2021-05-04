2116cc3e52c59173d80714763788c16cbcffffa6
Samuel Repka <xrepka07@stud.fit.vutbr.cz>
2021-05-04 11:03:45 +0200

    gui boxes refactoring

--- src/gui/ClassesGui/GuiWindow.cpp
+++ src/gui/ClassesGui/GuiWindow.cpp
@@ -9,25 +9,25 @@
 
 GuiWindow::GuiWindow(rayEngine *engine)
 {
-	create_info();
-	create_editor();
-	create_selector();
-	create_3d_view();
+	auto info_box = create_info();
+	auto editor_box = create_editor();
+	auto selector_box = create_selector();
+	auto view_3d_box = create_3d_view();
 	
 	engine_ = engine;
 
 	auto main_layout = new QGridLayout;
-	main_layout->addWidget(view_3d_widget_,0,0,5,5);
-	main_layout->addWidget(selector_,0,5,2,1);
-	main_layout->addWidget(editor_, 2, 5, 2, 1);
-	main_layout->addWidget(info_,4,5);
+	main_layout->addWidget(view_3d_box,0,0,5,5);
+	main_layout->addWidget(selector_box,0,5,2,1);
+	main_layout->addWidget(editor_box, 2, 5, 2, 1);
+	main_layout->addWidget(info_box,4,5);
 	
 	auto central_widget = new QWidget;
 	central_widget->setLayout(main_layout);
 	setCentralWidget(central_widget);
 }
 
-void GuiWindow::create_3d_view()
+QWidget* GuiWindow::create_3d_view()
 {
 	// 3D view
 	view_3d_ = new Qt3DExtras::Qt3DWindow;
@@ -51,32 +51,40 @@ void GuiWindow::create_3d_view()
 
 	add_camera(*view_3d_, root_entity);
 	view_3d_->setRootEntity(root_entity);
+
+	return view_3d_widget_;
 }
 
-void GuiWindow::create_editor()
+QGroupBox* GuiWindow::create_editor()
 {
-	editor_ = new QGroupBox;
-	editor_->setTitle(tr("GuiLens control."));
+	auto e_box = new QGroupBox;
+	e_box->setTitle(tr("GuiLens control."));
 	
-	auto lens_editor = new LensEditor;
+	editor_ = new LensEditor;
 	
 	auto vbox = new QVBoxLayout;
-	vbox->addWidget(lens_editor);
-	editor_->setLayout(vbox);
+	vbox->addWidget(editor_);
+	e_box->setLayout(vbox);
 
+	return e_box;
 }
 
-void GuiWindow::create_selector()
+QGroupBox* GuiWindow::create_selector()
 {
-	selector_ = new QGroupBox;
-	selector_->setTitle(tr("Lens editor."));
+	auto s_box = new QGroupBox;
+	s_box->setTitle(tr("Lens editor."));
 
-	auto lens_list = new LensList;
-	auto w = new QWidget();
-	lens_list->setParent(w);
+	selector_ = new LensList;
 	auto* layout = new QVBoxLayout;
-	layout->addWidget(w);
-	selector_->setLayout(layout);
+
+	// LensList is not working correctly in global layout, unless I wrap it in a widget
+	const auto selector_wrapper = new QWidget();
+	selector_->setParent(selector_wrapper);
+	
+	layout->addWidget(selector_wrapper);
+	s_box->setLayout(layout);
+
+	return s_box;
 }
 
 
@@ -86,16 +94,20 @@ void GuiWindow::button_clicked()
 }
 
 
-void GuiWindow::create_info()
+QGroupBox* GuiWindow::create_info()
 {
-	info_ = new QGroupBox;
-	info_->setTitle(tr("Informations about rays on detector."));
+	auto i_box = new QGroupBox;
+	i_box->setTitle(tr("Informations about rays on detector."));
 
 	auto button = new QPushButton("New", this);
 	connect(button, SIGNAL(clicked()), this, SLOT(button_clicked_()));
+	
 	QVBoxLayout* vbox = new QVBoxLayout;
 	vbox->addWidget(button);
-	info_->setLayout(vbox);
+	i_box->setLayout(vbox);
+
+	info_ = i_box; // TODO temporary
+	return i_box;
 }
 
 
--- src/gui/ClassesGui/GuiWindow.h
+++ src/gui/ClassesGui/GuiWindow.h
@@ -3,6 +3,7 @@
 #include <Qt3DExtras>
 #include <QtWidgets>
 
+#include "LensEditor.h"
 #include "LensList.h"
 #include "rayTracer/RayEngine.h"
 
@@ -21,25 +22,25 @@ private:
 	 * Constructs 3D viewer and scene inside.
 	 * Reference will be stored in view_3d_widget_
 	 */
-	void create_3d_view();
+	QWidget* create_3d_view();
 	
 	/**
 	 * Creates lens editing part of the window.
 	 * Reference will be stored in editor_
 	 */
-	void create_editor();
+	QGroupBox* create_editor();
 
 	/**
 	 * Creates part of the window with all lenses listed.
 	 * Reference will be stored in selector_
 	 */
-	void create_selector();
+	QGroupBox* create_selector();
 
 	/**
 	 * Creates informative part of the window.
 	 * Reference will be stored in info_
 	 */
-	void create_info();
+	QGroupBox* create_info();
 
 	
 	Qt3DCore::QEntity* create_scene();
@@ -48,8 +49,8 @@ private:
 
 	QWidget* view_3d_widget_;
 	Qt3DExtras::Qt3DWindow* view_3d_;
-	QGroupBox* selector_;
-	QGroupBox* editor_;
+	LensList* selector_;
+	LensEditor* editor_;
 	QGroupBox* info_;
 	QWidget *central_widget_;
 	rayEngine *engine_;
