095dfc6572d2d84269cb59435cfcb817334697a2
Samuel Repka <xrepka07@stud.fit.vutbr.cz>
2021-05-09 23:37:52 +0200

    LensSelectorItem refactored

--- src/gui/ClassesGui/LensSelectorItem.h
+++ src/gui/ClassesGui/LensSelectorItem.h
@@ -1,10 +1,25 @@
 #pragma once
 #include <QListWidgetItem>
 
+/**
+ * @brief Item for LensSelector.
+ */
 class LensSelectorItem : public QListWidgetItem
 {
 public:
+	/**
+	 * @brief Constructor.
+	 * 
+	 * @param id Id of the lens
+	 * @param label Label to be shown
+	 */
 	LensSelectorItem(int id, QString label);
+
+	/**
+	 * @brief id_ getter.
+	 * 
+	 * @return Id
+	 */
 	int getId() const;
 
 private:
