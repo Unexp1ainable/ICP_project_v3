cb45929248c0306e459be1d08c9dede902747ff5
Samuel Repka <xrepka07@stud.fit.vutbr.cz>
2021-05-11 14:37:00 +0200

    maybe

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include "src/gui/ClassesGui/GuiWindow.h"
 #include "src/rayTracer/RayEngine.h"
 
+#include <clocale>
+
 int main(int argc, char *argv[])
 {
+    std::setlocale(LC_NUMERIC, "C");
     QApplication app(argc, argv);
     rayEngine engine{ 15., 30., 50. };
     GuiWindow window(&engine);
