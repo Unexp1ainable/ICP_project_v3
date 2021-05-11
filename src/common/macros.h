c43ba24109c0ca7b3ac0880655f13586421db8aa
Samuel Repka <xrepka07@stud.fit.vutbr.cz>
2021-05-11 13:10:40 +0200

    constexpressions

--- src/common/macros.h
+++ src/common/macros.h
@@ -1,13 +1,12 @@
 #pragma once
-#define PI 3.141592653589793238463f
-#define TO_DEGREES(x) (x * (180.0f/PI))
-#define TO_RADIANS(x) (x*PI/180.0f)
+constexpr double PI = 3.141592653589793238463;
+template<typename T> constexpr auto TO_DEGREES(T x) { return x * (180.0 / PI); }
+template<typename T> constexpr auto TO_RADIANS(T x) { return x * PI / 180.0; }
 
-#define DETECTOR_SIZE 2
-#define SAMPLE_SIZE 10
+constexpr int DETECTOR_SIZE = 2;
+constexpr int SAMPLE_SIZE = 10;
+constexpr int LENS_SIZE = 10;
+constexpr int SOURCE_SIZE = 1;
 
-#define LENS_SIZE 10
-constexpr double SOURCE_SIZE =1;
-
-#define INIT_RAY_RADIUS 1.
-#define INIT_RAY_COUNT 10
+constexpr double INIT_RAY_RADIUS = 1.;
+constexpr double INIT_RAY_COUNT = 10;
